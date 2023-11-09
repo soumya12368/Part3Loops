91fd37145e7d26656001fecda3214a1ce4987691
Soumya Sutar <soumsutar9@gmail.com>
2023-11-09 15:31:59 +0530

    Part3Loop

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part3Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 10;
+            int[] fibonacci = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                if (i <= 1)
+                {
+                    fibonacci[i] = i;
+                }
+                else
+                {
+                    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                }
+                Console.Write(fibonacci[i] + " ");
+            }
+        }
+    }
+}
